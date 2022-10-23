6090da59d28502b902f084bcc34896a1ab76eb39
1 <1.com>
2022-10-23 15:02:26 +0800

    在有序数组中找到7

--- Project21/Project21/源.c
+++ Project21/Project21/源.c
@@ -12,7 +12,7 @@ void main()
 			num = 0;
 		}
 	}
-}*/
+}
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main()
@@ -20,11 +20,32 @@ int main()
 	int i = 0;
 	int n = 0;
 	int ret = 1;
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+	int sum=0;
+	for (n = 1; n <= 3; n++)
 	{
-		ret = ret*i;
+		ret = ret * n;
+		sum = sum + ret;
 	}
-	printf("ret=%d\n", ret);
+	printf("sum=%d\n", sum);
+	return 0;
+}*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	//写一个代码，在arr数组（有序的）中找到7
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		if (k == arr[i])
+		{
+			printf("找到了，下标是:%d\n", i);
+		}
+	}
+	if (i == sz)
+		printf("找不到\n");
 	return 0;
 }
